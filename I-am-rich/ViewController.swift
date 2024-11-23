297043dc3848ae05db48023491ab33000e309304
jun <developer@junmike.dev>
2024-11-23 03:22:43 -0600

    Initial Commit

---
+++ I-am-rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-am-rich
+//
+//  Created by jun on 11/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
